ae783cef67ca12abc75a8428f5861fd691934801
Siddwap <58450503+Siddwap@users.noreply.github.com>
2022-10-16 16:28:34 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
 # build_files.sh
-pip install requirements.txt
+pip freeze > requirements.txt
+find -name "requirements.txt"
+find . -regex '.*requirements.txt$' //on theroot directory of your terminal
+pip install -r requirements.txt
 python3.9 manage.py collectstatic
